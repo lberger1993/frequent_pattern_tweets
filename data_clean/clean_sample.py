7951e810b7c1db1fa4eca5a6e33ca4ff7e4b6dbd
lberger1993 <berger.luciaeve@gmail.com>
2017-11-02 20:38:55 +0100

    adds sample method for @ and removal

--- data_clean/clean_sample.py
+++ data_clean/clean_sample.py
@@ -8,11 +8,26 @@ def remove_stop_words(df):
     df['tweet'] = df['tweet'].apply(lambda x: [item for item in x if item not in get_stop_words()])
     return df
 
+def remove_user_names(df):
+    for index, row in df.iterrows():
+        for elem in row['tweet']:
+            if 'http' in elem:
+                row['tweet'].remove(elem)
+            if '@' in elem:
+                row['tweet'].remove(elem)
+    return df
+
+
 
 if __name__ == '__main__':
     all_tweets = pd.DataFrame.from_csv('data_sources/tweets.csv', index_col=None)
-    all_tweets['tweet'] = all_tweets['tweet'].str.lower().str.split()
+    # removes all punctuation & tweets
+    all_tweets['tweet'] = all_tweets['tweet'].str.lower().str.replace('[^\w\s]', '').str.split()
     all_tweets = remove_stop_words(all_tweets)
+    # all_tweets = remove_all_punc(all_tweets)
+    #print(all_tweets)
+    # all_tweets = remove_user_names(all_tweets)
+    # print(all_tweets['tweet'][11])
 
 
 
