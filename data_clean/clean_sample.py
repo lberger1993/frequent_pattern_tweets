d6064635a703a29f7249598e215d4c0a4d992049
lberger1993 <berger.luciaeve@gmail.com>
2017-11-02 20:25:58 +0100

    cleans up the removal

--- data_clean/clean_sample.py
+++ data_clean/clean_sample.py
@@ -5,21 +5,17 @@ from data_sources.stop_words import get_stop_words
 
 
 def remove_stop_words(df):
-    """ removes all stop words from row"""
-    for index, row in df.iterrows():
-        split = row['tweet'].split(" ")
-        for s in split:
-            if s in get_stop_words():
-                split.remove(s)
-        fusion = ' '.join(split)
-        row['tweet'] = fusion
+    df['tweet'] = df['tweet'].apply(lambda x: [item for item in x if item not in get_stop_words()])
+    return df
 
 
 if __name__ == '__main__':
     all_tweets = pd.DataFrame.from_csv('data_sources/tweets.csv', index_col=None)
-    all_tweets['tweet'] = all_tweets['tweet'].str.lower()
-    remove_stop_words(all_tweets)
-    print(all_tweets['tweet'])
+    all_tweets['tweet'] = all_tweets['tweet'].str.lower().str.split()
+    all_tweets = remove_stop_words(all_tweets)
+
+
+
 
 
 
--- data_clean/data_sources/stop_words.py
+++ data_clean/data_sources/stop_words.py
@@ -10,7 +10,7 @@ def get_stop_words():
                 "every","for","from","get","got","had",
                 "has","have","he","her","hers","him","his",
                 "how","however","i","if","in","into","is","it",
-                "its","just","least","let","like","likely","may",
+                "its","it's", "just","least","let","like","likely","may",
                 "me","might","most","must","my","neither","no","nor",
                 "not","of","off","often","on","only","or","other","our",
                 "own","rather","said","say","says","she","should","since",
